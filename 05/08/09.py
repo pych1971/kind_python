3220af57a2f8c83514f4dd040add11f123528c1e
pych1971 <pych1971@gmail.com>
2024-04-02 21:56:59 +0500

    2024-04-02-21-56

--- 05/08/09.py
+++ 05/08/09.py
@@ -1,1 +1,3 @@
-cities = []
+lst_tmp = [[x] if i % 2 == 0 else [int(x)] for i, x in enumerate(input().split())]
+lst = [lst_tmp[i] + lst_tmp[i + 1] for i in range(len(lst_tmp)) if i % 2 == 0]
+print(lst)
