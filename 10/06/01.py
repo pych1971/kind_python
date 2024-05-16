115032fc218c09406e628ab98152bb54f8a0ab60
pych1971 <pych1971@gmail.com>
2024-05-16 15:34:26 +0500

    2024-05-16-15-34

---
+++ 10/06/01.py
@@ -0,0 +1,14 @@
+def parse_json(data):
+    match data:
+        # здесь прописывайте шаблон
+        case {'access': bool(access), 'data': list([(date), *_])}:
+            return (access, date)
+        case {'id': ids, 'data': [_, {'login': login}, _, _]}:
+            return ids, login
+
+    return None
+
+
+json_data = {'id': 2, 'access': False, 'data': ['26.05.2023', {'login': '1234', 'email': 'xxx@mail.com'}, 2000, 56.4]}
+
+print(parse_json(json_data))
